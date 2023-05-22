f44a4a33f138456fbc3b8add03389c67d816a973
Rodion Sviridov <sviridov.rod@yandex.ru>
2023-05-22 18:09:40 +0300

    DI modules for repositories and viewModels updated

--- app/src/main/java/com/rodyapal/gigachads/di/RepositoryModule.kt
+++ app/src/main/java/com/rodyapal/gigachads/di/RepositoryModule.kt
@@ -1,8 +1,16 @@
 package com.rodyapal.gigachads.di
 
+import com.rodyapal.gigachads.model.repository.CommentRepository
+import com.rodyapal.gigachads.model.repository.GameRepository
+import com.rodyapal.gigachads.model.repository.PostRepository
+import com.rodyapal.gigachads.model.repository.ServerRepository
 import com.rodyapal.gigachads.model.repository.UserRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
 	single { UserRepository(get()) }
+	single { ServerRepository(get(), get()) }
+	single { CommentRepository(get(), get()) }
+	single { GameRepository(get(), get()) }
+	single { PostRepository(get(), get(), get(), get()) }
 }
--- app/src/main/java/com/rodyapal/gigachads/di/ViewModelModule.kt
+++ app/src/main/java/com/rodyapal/gigachads/di/ViewModelModule.kt
@@ -1,9 +1,21 @@
 package com.rodyapal.gigachads.di
 
+import com.rodyapal.gigachads.screens.favservers.FavoriteServersViewModel
 import com.rodyapal.gigachads.screens.login.LoginViewModel
+import com.rodyapal.gigachads.screens.post.PostViewModel
+import com.rodyapal.gigachads.screens.register.RegisterViewModel
+import com.rodyapal.gigachads.screens.search.SearchViewModel
+import com.rodyapal.gigachads.screens.server.ServerViewModel
+import com.rodyapal.gigachads.screens.serverposts.ServerPostsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+	viewModel { FavoriteServersViewModel(get(), get(), get()) }
 	viewModel { LoginViewModel(get()) }
+	viewModel { PostViewModel(get(), get(), get(), get()) }
+	viewModel { RegisterViewModel(get()) }
+	viewModel { SearchViewModel(get(), get()) }
+	viewModel { ServerViewModel(get(), get(), get(), get()) }
+	viewModel { ServerPostsViewModel(get(), get(), get()) }
 }
